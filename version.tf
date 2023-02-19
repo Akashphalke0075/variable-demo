01eae48f9fcbef2625bee52dc65bab697c7c94ca
Akash <phalke.aka@gmail.com>
2023-02-19 15:02:34 +0530

    update

--- version.tf
+++ version.tf
@@ -1,14 +1,14 @@
-terraform {
-  required_version = ">= 0.12"
-  required_providers {
-    aws = {
-        source = "Hashicorp/aws"
-        version = "4.52.0"
-    }
-  }
+# terraform {
+#   required_version = ">= 0.12"
+#   required_providers {
+#     aws = {
+#         source = "Hashicorp/aws"
+#         version = "4.52.0"
+#     }
+#   }
   
-}
+# }
 
-provider "aws" {
-    region = var.aws_region
-}
+# provider "aws" {
+#     region = var.aws_region
+# }
