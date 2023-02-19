f5b74d73b509b09b42d4079b88e9354da36d18d9
Akash <phalke.aka@gmail.com>
2023-02-19 14:20:07 +0530

    update

--- instance.tf
+++ instance.tf
@@ -1,32 +1,32 @@
-resource "aws_instance" "class-nginx" {
-    ami = var.aws_ami
-    instance_type = var.instance_type
-    vpc_security_group_ids = ["aws_security_group.new-security-sg.id"]
-
-    user_data = <<-EOF
-        #!/bin/bash
-        sudo apt update -y &&
-        sudo apt install -y nginx
-        echo "<h1>Welcome to Terraform nginx-demo</h1>" > /var/www/html/index.html
-        EOF
-  tags = {
-    "Name" = var.aws_name
-  }
-}
-
 # resource "aws_instance" "class-nginx" {
-#   ami = var.aws_ami
-#   instance_type = var.instance_type
-#   vpc_security_group_ids = ["aws_security_group.new-security-sg.id"]
+#     ami = var.aws_ami
+#     instance_type = var.instance_type
+#     vpc_security_group_ids = ["aws_security_group.new-security-sg.id"]
 
+#     user_data = <<-EOF
+#         #!/bin/bash
+#         sudo apt update -y &&
+#         sudo apt install -y nginx
+#         echo "<h1>Welcome to Terraform nginx-demo</h1>" > /var/www/html/index.html
+#         EOF
 #   tags = {
-#     Name = var.aws_name
-  
+#     "Name" = var.aws_name
 #   }
-#   user_data = <<-EOF
-#   #!/bin/bash
-#   sudo apt update -y &&
-#   sudo apt install -y nginx
-#   echo "<h1>Welcome to Terraform akash nginx-demo</h1>" > /var/www/html/index.html
-#   EOF
 # }
+
+resource "aws_instance" "class-nginx" {
+  ami = var.aws_ami
+  instance_type = var.instance_type
+  vpc_security_group_ids = ["aws_security_group.new-security-sg.id"]
+
+  tags = {
+    Name = var.aws_name
+  
+  }
+  user_data = <<-EOF
+  #!/bin/bash
+  sudo apt update -y &&
+  sudo apt install -y nginx
+  echo "<h1>Welcome to Terraform akash nginx-demo</h1>" > /var/www/html/index.html
+  EOF
+}
