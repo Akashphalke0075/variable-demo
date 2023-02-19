b2fda60b4622103118cbf36806a6101e767ac983
Akash <phalke.aka@gmail.com>
2023-02-19 15:08:25 +0530

    update

--- 1security.tf
+++ 1security.tf
@@ -1,6 +1,6 @@
 resource "aws_security_group" "class-sg-server" {
     name = "class-security-new"
-    acl = "Allow SSH Access"
+    description = "EC2 Security group"
 
     tags = {
         Name = "class-security-group"
@@ -18,14 +18,14 @@ resource "aws_security_group" "class-sg-server" {
         to_port = 80
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
+    }
 
     egress {
-        ipv6_cidr_blocks = ["::/0"]
-        cidr_blocks = ["0.0.0.0/0"]
-        from_port = 0
-        to_port = 0
-        protocol = -1
-
-    }   
-    }
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+    from_port        = 0
+    to_port          = 0
+    protocol         = -1
+  }  
+    
 }
