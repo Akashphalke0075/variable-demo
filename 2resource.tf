0d15fa8c96d776dda0e05f761d81510357d73a00
Akash <phalke.aka@gmail.com>
2023-02-20 17:44:09 +0530

    update

--- 2resource.tf
+++ 2resource.tf
@@ -1,17 +1,15 @@
-resource "aws_instance" "class-server" {
-    ami = "ami-0b93ce03dcbcb10f6"
-    instance_type = "t2.micro"
-    vpc_security_group_ids = ["aws_security_group.class-sg.id"]
-
-    tags = {
-        Name = "ec2-server"
-    }
-
-    user_data = <<-EOF
-  #!/bin/bash
-  sudo apt update -y &&
-  sudo apt install -y nginx
-  echo "<h1>Welcome to Terraform akash nginx-demo</h1>" > /var/www/html/index.html
-  EOF
-
+resource "aws_instance" "class-ec2-server" {
+  instance_type = "t2.micro" #Refer value from ec2_instance_type variable
+  ami = "ami-0b93ce03dcbcb10f6"
+  vpc_security_group_ids = [aws_security_group.class-sg.id]
+  #key_name = "jenkins-slave"
+  user_data = <<-EOF
+        #!/bin/bash
+        sudo apt update -y &&
+        sudo apt install -y nginx
+        echo "<h1>Welcome to Terraform nginx-demo</h1>" > /var/www/html/index.html
+        EOF
+  tags = {
+    "Name" = "new-ec2-serv"
+  }
 }
